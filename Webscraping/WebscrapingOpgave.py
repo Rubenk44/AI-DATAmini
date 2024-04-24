9deeeae9cab4462a996b5cbe9d70ff7a69d1b458
Rubenk44 <95618435+Rubenk44@users.noreply.github.com>
2024-04-24 16:52:19 +0200

    yeye

---
+++ Webscraping/WebscrapingOpgave.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+url = "http://olympus.realpython.org/profiles/aphrodite"
+#for at åbne hjemmesiden
+page = input()
+
+def webscraper(page):
+    
+ pass
+
+def main():
+    pass
+    
